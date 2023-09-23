aaeea171b60f3ddeb41ab20bd9e385d5b7d306ce
Prof-Accord <sh.watanabe94@gmail.com>
2023-09-23 12:36:35 +0900

    静的ページを作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
+  let(:base_title) { '| mytodo' }
+
+  describe "homeページ" do
+    it "アクセスに成功すること" do
+      get root_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Home #{base_title}"
+      
     end
   end
 
-  describe "GET /about" do
-    it "returns http success" do
+  describe "aboutページ" do
+    it "アクセスに成功すること" do
       get "/static_pages/about"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "About #{base_title}"
+    end
+  end
+
+  describe "helpアクション" do
+    it "アクセスに成功すること" do
+      get "/static_pages/help"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include "Help #{base_title}"
     end
   end
 
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "static_pages home" do
+    skip
+  end
 end
