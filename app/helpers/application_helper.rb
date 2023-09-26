1176a5719324724271c6fdea2c773ace66dc7b8c
Prof-Accord <sh.watanabe94@gmail.com>
2023-09-26 14:11:02 +0900

    full_titleメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = "")
+    base_title = "todo"
+    if page_title.empty?
+      p base_title
+    else
+      p "#{page_title} | #{base_title}"
+    end
+  end
 end
