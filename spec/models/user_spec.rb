a903fda0ce012401e028d63ce9eb3bcaf88bd914
Prof-Accord <sh.watanabe94@gmail.com>
2023-10-04 14:40:31 +0900

    _headerパーシャルを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "Userモデルのテスト" do
-    it "emailのバリデーション" do
-      user = User.new(name: "",
-                      email: "",
-                      password: "password",
-                      password_confirmation: "password")
-      expect(user).to be_invalid
+  describe User do
+    before do
+      @user = build(:user)
+    end
+    context "nameのバリデーション" do
+      it "有効である" do
+        expect(@user).to be_valid
+      end
+      it "存在しなくてはならない" do
+        @user.name = "    "
+        expect(@user).to be_invalid
+      end
+      it "50文字以下である" do
+        @user.name = "a" * 51
+        expect(@user).to be_invalid
+      end
+    end
+    context "emailのバリデーション" do
+      it "存在しなくてはならない" do
+        @user.email = "   "
+        expect(@user).to be_invalid
+      end
+      it "50文字以下である" do
+        @user.name = "a" * 244 + "example.com"
+        expect(@user).to be_invalid
+      end
     end
   end
 end
